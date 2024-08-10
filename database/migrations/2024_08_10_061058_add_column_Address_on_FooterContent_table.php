04c6a7a7b2eeb8920a2c6bf1d304263f4d202869
sifani affan <affan.sekawanmedia@gmail.com>
2024-08-10 13:16:34 +0700

    add migration add column address footer content

---
+++ database/migrations/2024_08_10_061058_add_column_Address_on_FooterContent_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('footercontent', function (Blueprint $table) 
+        {
+            $table->string('FooterContentAddress', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('footercontent', function (Blueprint $table) {
+            try {
+                $table->dropColumn('FooterContentAddress');
+            } catch (Exception $e) {
+                Log::error($e->getMessage());
+                throw new \RuntimeException($e->getMessage());
+            }
+        });
+    }
+};
