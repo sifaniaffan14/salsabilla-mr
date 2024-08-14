4a59d6ca2c05b74136d46653e966b16be80bfd19
sifani affan <affan.sekawanmedia@gmail.com>
2024-08-14 16:02:44 +0700

    except route get out of athorize

--- routes/api.php
+++ routes/api.php
@@ -30,17 +30,35 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/update-token', [AuthController::class, 'generateToken']);
 
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{productId}', [ProductController::class, 'show']);
+
+Route::get('/footer-contents', [FooterContentController::class, 'index']);
+Route::get('/footer-contents/{footerContentId}', [FooterContentController::class, 'show']);
+
+Route::get('/jumbotron', [JumbotronSettingController::class, 'index']);
+Route::get('/jumbotron/{jumbotronId}', [JumbotronSettingController::class, 'show']);
+
+Route::get('/about-us', [AboutUsSettingController::class, 'index']);
+Route::get('/about-us/{aboutUsId}', [AboutUsSettingController::class, 'show']);
+
+Route::get('/social-media', [SocialMediaController::class, 'index']);
+Route::get('/social-media/{socialMediaId}', [SocialMediaController::class, 'show']);
+
+Route::get('/product-details', [ProductDetailController::class, 'index']);
+Route::get('/product-details/{productDetailId}', [ProductDetailController::class, 'show']);
+
 Route::middleware([EnsureTokenIsValid::class])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
-    Route::apiResource('/products', ProductController::class)->except('update');
+    Route::apiResource('/products', ProductController::class)->except('update','index', 'show');
     Route::post('/products/{productId}', [ProductController::class, "update"]);
-    Route::apiResource('/footer-contents', FooterContentController::class);
-    Route::apiResource('/jumbotron', JumbotronSettingController::class)->except('update');
+    Route::apiResource('/footer-contents', FooterContentController::class)->except('update','index', 'show');
+    Route::apiResource('/jumbotron', JumbotronSettingController::class)->except('update','index', 'show');
     Route::post('/jumbotron/{jumbotronId}', [JumbotronSettingController::class, "update"]);
-    Route::apiResource('/about-us', AboutUsSettingController::class)->except('update');
+    Route::apiResource('/about-us', AboutUsSettingController::class)->except('update','index', 'show');
     Route::post('/about-us/{aboutUsId}', [AboutUsSettingController::class, "update"]);
-    Route::apiResource('/social-media', SocialMediaController::class);
+    Route::apiResource('/social-media', SocialMediaController::class)->except('index', 'show');
     Route::apiResource('/product-details', ProductDetailController::class);
     Route::apiResource('/users', ConfigUserController::class)->except('update');
     Route::post('/users/{userId}', [ConfigUserController::class, "update"]);
