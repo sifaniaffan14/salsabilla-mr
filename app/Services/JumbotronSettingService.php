58e3315fa1806cdc48c597a1e6b25c18d87ef6d0
sifani affan <affan.sekawanmedia@gmail.com>
2024-08-19 16:48:30 +0700

    update jumbotron create

--- app/Services/JumbotronSettingService.php
+++ app/Services/JumbotronSettingService.php
@@ -31,7 +31,7 @@ class JumbotronSettingService extends BaseService
 
             $image = $data['JumbotronImage'] ?? null;
             if ($image) {
-                $imageName = 'jumbotron_' . $data['JumbotronTittle'] . '_' . uniqid() . '.' . $image->getClientOriginalExtension();
+                $imageName = 'jumbotron_' . uniqid() . '.' . $image->getClientOriginalExtension();
                 $image->move(public_path('storage/image/jumbotron'), $imageName);
                 $data['JumbotronImage'] = json_encode('/storage/image/jumbotron/' . $imageName);
             }
